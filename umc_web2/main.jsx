04a58889d18d49e75e55cb806398cee3da580312
E-hyeon <165488194+E-hyeon@users.noreply.github.com>
2024-04-09 15:06:06 +0900

    Add files via upload

--- umc_web2/main.jsx
+++ umc_web2/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './umc2_1_count/App.jsx'
-
+import App from './umc2_2_modal/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
